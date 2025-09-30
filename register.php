db8daea55a415e0686825ce95a9837bc4b8f1ded
Loyiso Ngqwebo <ngqwebol@gmail..com>
2025-09-30 12:04:17 +0200

    Documentation updates

--- register.php
+++ register.php
@@ -1,20 +1,20 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "Deliver Files for Students",
-"FontAwesome" => "fa-file-text-o",
-"short_name" => "File Delivery",
-"description" => "",
+    "name" => "Deliver Files for Students",
+    "FontAwesome" => "fa-file-text-o",
+    "short_name" => "File Delivery",
+    "description" => "Site owners and lecturers can upload files to the site and students will then gain access to particular files that contain their student number in the filename.",
     "messages" => array("launch", "launch_grade"),
     "privacy_level" => "anonymous",  // anonymous, name_only, public
-    "license" => "Apache",
+    "license" => "Apache License 2.0",
     "languages" => array(
         "English"
     ),
     "analytics" => array(
         "internal"
     ),
-    // "source_url" => "https://github.com/tsugitools/iframe",
+    "source_url" => "https://github.com/cilt-uct/tsugi-deliver-student-files",
     // For now Tsugi tools delegate this to /lti/store
     "placements" => array(
         /*
@@ -28,5 +28,4 @@ $REGISTER_LTI2 = array(
         // "store/screen-03.png",
         // "store/screen-analytics.png"
     )
-
 );
